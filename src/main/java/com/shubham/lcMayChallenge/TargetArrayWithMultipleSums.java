7f8a93ce9ab9b3772deccb658671be7038926841
Shubham Agrawal <agrawalshubham1103@gmail.com>
2021-05-15 11:12:12 -0400

    Leetcode May 2nd Week

---
+++ src/main/java/com/shubham/lcMayChallenge/TargetArrayWithMultipleSums.java
@@ -0,0 +1,30 @@
+package com.shubham.lcMayChallenge;
+
+import java.util.PriorityQueue;
+
+public class TargetArrayWithMultipleSums {
+
+  public boolean isPossible(int[] target) {
+    PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> (b - a));
+    long total = 0;
+    for (int n : target) {
+      total += n;
+      queue.add(n);
+    }
+    while (!queue.isEmpty()) {
+      int a = queue.poll();
+      total -= a;
+      if (a == 1 || total == 1) {
+        return true;
+      }
+      if (a < total || total == 0 || a % total == 0) {
+        return false;
+      }
+      a %= total;
+      total += a;
+      queue.add(a);
+    }
+    return true;
+  }
+
+}
